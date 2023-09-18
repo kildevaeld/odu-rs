ee2844019e2aacac873d1a086c751fea3e090153
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2023-09-18 22:54:08 +0200

    impl partialeq for signature

--- odu-func/src/signature/sign.rs
+++ odu-func/src/signature/sign.rs
@@ -2,7 +2,7 @@ use super::Parameters;
 use odu_types::{PrimitiveType, Type};
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Signature {
     params: Parameters,
     return_type: Type,
