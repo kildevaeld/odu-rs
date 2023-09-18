017b775c0cca798cb3d7e7da2c1cedc52d0f1147
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2023-09-18 16:09:46 +0200

    remove unsued

--- odu-types/examples/serde-types.rs
+++ odu-types/examples/serde-types.rs
@@ -31,11 +31,7 @@ fn main() {
 
     let o: ComplexType = serde_json::from_str(INPUT).expect("msg");
 
-    // println!("{:#?}", odu_types::type_info(ty));
-
-    //let o = serde_json::to_string_pretty(&ty).expect("serde");
-
-    // println!("{:?} {}", ustr::total_allocated(), ustr::total_capacity());
+    
   println!("types len: {}", odu_types::types_len());
 
     std::thread::sleep(std::time::Duration::from_secs(1000))
